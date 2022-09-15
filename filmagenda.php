bcbde02ff2adb5332002d8d78c00cf5745895c3b
smschijf <210578@student.glu.nl>
2022-09-15 14:51:45 +0200

    mustache werkend + data ophalen api werkend

--- filmagenda.php
+++ filmagenda.php
@@ -1,5 +1,20 @@
 <?php
 require 'header.php';
+require __DIR__ . '/vendor/autoload.php';
+
+$rawJSONData = file_get_contents('assets/json/data.json');
+$jsonData = json_decode($rawJSONData, true);
+
+//instance mustache
+$mustache = new Mustache_Engine(array('entity_flags' => ENT_QUOTES, 'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/templates')));
+//load mustache template
+$movieBlockTemplate = $mustache->loadTemplate('movieBlock');
+?>
+<?php
+for ($i = 0; $i < ($jsonData["filmdata"]); $i++) {
+  //render mustache template and insert data from jsonData
+  echo $movieBlockTemplate->render($jsonData["filmdata"][$i]);
+}
 ?>
 <?php
 require 'footer.php';
