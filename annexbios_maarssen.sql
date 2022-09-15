5896300e73bf7f5cfc556a4860f2a1f321dfabf0
smschijf <210578@student.glu.nl>
2022-09-15 13:21:54 +0200

    database dump added

--- annexbios_maarssen.sql
+++ annexbios_maarssen.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Sep 08, 2022 at 12:44 PM
+-- Generation Time: Sep 15, 2022 at 11:20 AM
 -- Server version: 5.7.34
 -- PHP Version: 8.0.8
 
@@ -31,9 +31,29 @@ CREATE TABLE `Klant` (
   `id` int(9) NOT NULL,
   `voornaam` varchar(256) NOT NULL,
   `achternaam` varchar(256) NOT NULL,
-  `email` varchar(256) NOT NULL,
-  `reservering_id` int(9) NOT NULL
+  `email` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Klant`
+--
+
+INSERT INTO `Klant` (`id`, `voornaam`, `achternaam`, `email`) VALUES
+(1, 'Test', 'Test', 'test@123.nl'),
+(2, 'Test', 'Test', 'test@123.nl'),
+(3, 'Test', 'Test', 'test@123.nl'),
+(4, 'Test', 'Test', 'test@123.nl'),
+(5, 'Test', 'Test', 'test@123.nl'),
+(6, 'Test', 'Test', 'test@123.nl'),
+(7, 'Test', 'Test', 'test@123.nl'),
+(8, 'Test', 'Test', 'test@123.nl'),
+(9, 'Test', 'Test', 'test@123.nl'),
+(10, 'Test', 'Test', 'test@123.nl'),
+(11, 'Test', 'Test', 'test@123.nl'),
+(12, 'Test', 'Test', 'test@123.nl'),
+(13, 'Test', 'Test', 'test@123.nl'),
+(14, 'Test', 'Test', 'test@123.nl'),
+(15, 'Test', 'Test', 'test@123.nl');
 
 -- --------------------------------------------------------
 
@@ -43,13 +63,22 @@ CREATE TABLE `Klant` (
 
 CREATE TABLE `Reserveringen` (
   `id` int(9) NOT NULL,
-  `ticket_normaal` int(2) NOT NULL,
-  `ticket_kind` int(2) NOT NULL,
-  `ticket_65` int(2) NOT NULL,
-  `klant_id` int(9) NOT NULL,
-  `vertoning_id` int(9) NOT NULL,
+  `ticket_normaal` int(2) DEFAULT NULL,
+  `ticket_kind` int(2) DEFAULT NULL,
+  `ticket_65` int(2) DEFAULT NULL,
+  `klant_id` int(9) DEFAULT NULL,
+  `vertoning_id` int(9) DEFAULT NULL,
   `betaalwijze` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Reserveringen`
+--
+
+INSERT INTO `Reserveringen` (`id`, `ticket_normaal`, `ticket_kind`, `ticket_65`, `klant_id`, `vertoning_id`, `betaalwijze`) VALUES
+(1, 2, 0, 0, 13, NULL, '1'),
+(2, 2, 0, 0, 14, NULL, 'ideal'),
+(3, 2, 0, 0, 15, NULL, 'ideal');
 
 -- --------------------------------------------------------
 
@@ -95,13 +124,13 @@ ALTER TABLE `Vertoningen`
 -- AUTO_INCREMENT for table `Klant`
 --
 ALTER TABLE `Klant`
-  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `Reserveringen`
 --
 ALTER TABLE `Reserveringen`
-  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `Vertoningen`
