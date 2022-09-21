60144d633db34fa470b2681f31f64f802465efca
smschijf <210578@student.glu.nl>
2022-09-21 10:16:29 +0200

    form working again and correct redirect from informatiepagina

--- assets/php/tickets_bestellen.php
+++ assets/php/tickets_bestellen.php
@@ -4,5 +4,5 @@
 
   if(isset($_POST['afrekenen_submit'])) {
     afrekenen($_POST);
-    // header('Location: ../../index.html');
+    header('Location: ../../index.php');
   }
--- bestel.php
+++ bestel.php
@@ -8,7 +8,7 @@ require 'header.php';
   <h1>TICKETS BESTELLEN</h1>
   <ul class="datetime">
     <li>
-      <p class="moviename">Jurrasic World</p>
+      <p class="moviename"><?php echo $jsonData["filmdata"][0]["film_title"] ?></p>
     </li>
     <li><select name="dateoption" class="dateoption">
         <option selected="selected" value="datum">Datum</option>
@@ -19,7 +19,7 @@ require 'header.php';
         <option value="" name=""></option>
       </select></li>
   </ul>
-  <form method="post" class="orderform" action="#">
+  <form method="post" class="orderform" action="assets/php/tickets_bestellen.php">
     <!--MAIN FORM WHERE ALL ORDER STEPS WILL TAKE PLACE-->
     <div class="mainbox">
       <div class="stap1">
@@ -103,23 +103,22 @@ require 'header.php';
         <div class="stap4">
           <h2>STAP 4: VUL JE GEGEVENS IN</h2>
           <div class="information">
-            <input type="text" placeholder="Voormaan*" required="required">
-            <input type="text" placeholder="Achternam*" required="required">
-            <input type="email" placeholder="E-mailadres*" required="required">
-            <input type="email" placeholder="E-mailadres*" required="required">
+            <input type="text" placeholder="Voormaan*" name="voornaam" required>
+            <input type="text" placeholder="Achternam*" name="achternaam" required>
+            <input type="email" placeholder="E-mailadres*" name="email" required>
           </div>
         </div>
         <div class="stap5">
           <h2>STAP 5: KIES JE BETAALWIJZE</h2>
           <div class="paymentoptios">
-            <input type="checkbox" name="nbb"><img src="" alt="nbb">
-            <input type="checkbox" name="maestro"><img src="" alt="maestro">
-            <input type="checkbox" name="ideal"><img src="" alt="ideal">
-            <input type="checkbox" name="nbb">Ja, ik ga akkoord met de <a href="#" class="ticketlink">algemene voorwaarden</a></input>
+            <input type="checkbox" name="betaalwijze" ><img src="" alt="nbb">
+            <input type="checkbox" name="betaalwijze" ><img src="" alt="maestro">
+            <input type="checkbox" name="betaalwijze" ><img src="" alt="ideal">
+            <input type="checkbox" name="betaalwijze" required>Ja, ik ga akkoord met de <a href="#" class="ticketlink">algemene voorwaarden</a></input>
           </div>
         </div>
       </div>
-      <input type="submit" value="AFREKENEN" required="required" />
+      <input type="submit" value="AFREKENEN" name="afrekenen_submit" required />
   </form>
 
 </main>
